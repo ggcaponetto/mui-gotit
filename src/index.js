efa0d5901de28e909643e4a2533c7fcc62b8a609
Giuseppe Caponetto <giuseppe.caponetto@geoimpact.ch>
2021-10-12 19:50:40 +0200

    autobuild

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import { Gotit as GotitLib} from './gotit-pragma-automatic'
 import { GotitContext as GotitContextLib } from './gotit-pragma-automatic';
 
 export default {
-  MUIGotit: {
-    Gotit: GotitLib,
-    GotitContext: GotitContextLib
-  }
+  Gotit: GotitLib,
 };
 
+export default {
+  GotitContext: GotitContextLib,
+};
